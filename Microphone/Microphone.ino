f48e5a82ee056e8c3fafcc5e8dab81f3da0074a9
Mitzsam <msamson26@shulamithhs.org>
2024-04-08 15:56:59 -0400

    Added Flash and Buzz
    Now when it gets too loud it will flash and buzz

--- Microphone/Microphone.ino
+++ Microphone/Microphone.ino
@@ -1,11 +1,19 @@
-const int rightMic = A2;
-const int leftMic = A1;
+const int rightMic = A3;
+const int leftMic = A2;
 int rightMicState = 0;
 int leftMicState = 0;
+
+int buzzerPin = 9;
+int flashPin = 3;
+int flashDelay = 165;
+
+int loudnessThreshold = 300;
 
 void setup(){
   pinMode(rightMic, INPUT);
   pinMode(leftMic, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(flashPin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -17,6 +25,31 @@ void loop(){
   
   Serial.print("Left: ");
   Serial.println(leftMicState);
-delay(100);
+  delay(100);
+  digitalWrite(buzzerPin, HIGH);
 
+  if (rightMicState > loudnessThreshold || leftMicState > loudnessThreshold){
+    flashAndBuzz();
+    //message "Please refrain from screaming and maintain proper volume control"
+  }
+
+}
+
+void flashAndBuzz(){
+  tone(buzzerPin, 1000);
+
+  digitalWrite(flashPin, HIGH);
+  delay(flashDelay);
+  digitalWrite(flashPin, LOW);
+  delay(flashDelay);
+   digitalWrite(flashPin, HIGH);
+  delay(flashDelay);
+  digitalWrite(flashPin, LOW);
+  delay(flashDelay);
+   digitalWrite(flashPin, HIGH);
+  delay(flashDelay);
+  digitalWrite(flashPin, LOW);
+  digitalWrite(buzzerPin, LOW);
+
+  noTone(buzzerPin);
 }
